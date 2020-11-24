e17356665d1592ad11757a5f2a3bb4ed62697801
azurealstn <azurealstn@naver.com>
2020-11-24 12:38:05 +0900

    데이터 표출

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+    {
+        id: 1,
+        image: 'https://placeimg.com/64/64/any',
+        name: '채민수',
+        birthday: '950502',
+        gender: '남자',
+        job: '대학생',
+    },
+    {
+        id: 2,
+        image: 'https://placeimg.com/64/64/any',
+        name: '채민수22',
+        birthday: '950502222',
+        gender: '남자22',
+        job: '대학생22',
+    },
+    {
+        id: 3,
+        image: 'https://placeimg.com/64/64/any',
+        name: '채민수3',
+        birthday: '95050233',
+        gender: '남자33',
+        job: '대학생33',
+    },
+];
 
 function App() {
     return (
-        <div className="gray-background">
-            <img src={logo} alt="logo" />
-            <h2>Let's develop management develop</h2>
+        <div>
+            {customers.map((c) => {
+                return (
+                    <Customer
+                        key={c.id}
+                        id={c.id}
+                        image={c.image}
+                        name={c.name}
+                        birthday={c.birthday}
+                        gender={c.gender}
+                        job={c.job}
+                    />
+                );
+            })}
         </div>
     );
 }
