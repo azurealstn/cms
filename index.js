fd7eea8a311604ed7842550578dc1de38c49abe7
azurealstn <azurealstn@naver.com>
2020-11-24 23:27:25 +0900

    mysql connect

--- index.js
+++ index.js
@@ -2,37 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const fs = require('fs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connnection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connnection.connect();
+
+//mysql 로 데이터를 가져온다
 app.get('/api/customers', (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: 'https://placeimg.com/64/64/any',
-      name: '채민수',
-      birthday: '950502',
-      gender: '남자',
-      job: '대학생',
-    },
-    {
-      id: 2,
-      image: 'https://placeimg.com/64/64/any',
-      name: '채민수22',
-      birthday: '950502222',
-      gender: '남자22',
-      job: '대학생22',
-    },
-    {
-      id: 3,
-      image: 'https://placeimg.com/64/64/any',
-      name: '채민수3',
-      birthday: '95050233',
-      gender: '남자33',
-      job: '대학생33',
-    },
-  ]);
+  connnection.query('select * from customer', (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => {
